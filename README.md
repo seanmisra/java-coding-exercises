60e08968a9504d7ef5535ed774116927f120887b
seanmisra <seanmisra30@gmail.com>
2023-04-23 17:16:47 -0400

    Exercise 4: String concatenation order

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Commits correspond to each exercise
 1. Maven project Hello World
 2. write main() entry-point function in another way
 3. Demonstrate String immutability
+4. Importance of order in String concatenation
--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,10 +7,11 @@ package org.example;
 public class App 
 {
     static public void main( String... args ) {
-        String test = "hello";
-        String testTwo = test.concat(" world");
+        // concatenates left-to-right
+        String test = "1" + 2 + 3;
+        String testTwo = 1 + 2 + "3";
 
-        System.out.println(test); // still hello
-        System.out.println(testTwo); // hello world
+        System.out.println(test); // 123
+        System.out.println(testTwo); // 33
     }
 }
