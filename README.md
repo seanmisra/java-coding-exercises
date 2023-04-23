20e22d9a5a1d714fb77c7a7dd20d4c74054f733b
seanmisra <seanmisra30@gmail.com>
2023-04-23 17:40:02 -0400

    Exercise 9: String equality with equals()

--- README.md
+++ README.md
@@ -10,3 +10,4 @@ Commits correspond to each exercise
 6. How many primitive data types can equal 'a'?
 7. Demonstrate StringBuilder mutability (in contrast to String)
 8. String literal pool (use == to demonstrate equality)
+9. String equality with equals (use same examples from Exercise 8 for clarity)
--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,16 +10,16 @@ public class App
         String firstString = "hello";
         String secondString = "hello";
         String thirdString = new String("hello").intern();
-
-        if (firstString == secondString && secondString == thirdString) {
-            System.out.println("The above three Strings recycle the same reference on the String literal pool (on the heap)");
-        }
-
         String fourthString = "he".concat("llo");
         String fifthString = new String("hello");
 
-        if (firstString != fourthString && firstString != fifthString) {
-            System.out.println("The 4th and 5th strings do not refer to the String literal pool");
+
+        if (firstString.equals(secondString)
+            && secondString.equals(thirdString)
+            && thirdString.equals(fourthString)
+            && fourthString.equals(fifthString)
+        ) {
+            System.out.println("All Strings above are equal via String.equals()");
         }
     }
 }
